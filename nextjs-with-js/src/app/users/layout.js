3a0a623c7cde030931432fce818a2e8c60fc92e3
Ravinder Negi <r.singh.negi85@gmail.com>
2024-05-22 12:09:37 +0530

    Example nextjs 14 setup

---
+++ nextjs-with-js/src/app/users/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "User Layout",
+  description: "User Layout settings",
+};
+
+export default function RootLayout({ children }) {
+  return <section>{children}</section>;
+}
